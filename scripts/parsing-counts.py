6b80f71588fd99347c72b3923d810c902a81937d
Karina Robledo <Karina@asyade.local>
2016-02-08 14:13:49 -0600

    Se modifico parsing-counts.py

--- scripts/parsing-counts.py
+++ scripts/parsing-counts.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 data = {}
 cepas = []
-FH = open ("names.txt", "r")
+FH = open ("../names.txt", "r")
 for renglon in FH:
   CFH = open ("%s/abundance.tsv" % renglon.strip())
+  CFH.readline()
   cepas.append(renglon.strip()) #se hace el arreglo con el nombre de las cepas
   for linea in CFH: #rellena el diccionario con los genes y los conteos
-    items = line.strip().split("\t")
+    items = linea.strip().split("\t")
     if items[0] in data:
-      data[items[0]].append(int(items[3]))
+      data[items[0]].append(float(items[3]))
     else:
-      data[items[0]] = [int(items[3])]
+      data[items[0]] = [float(items[3])]
 OUT = open ("conteos-totales.tsv", "w+")
 OUT.write("id\t")
 for cepa in cepas:
@@ -19,10 +20,12 @@ OUT.write("\n")
 for key, values in data.iteritems():
   OUT.write("%s\t" % key)
   for value in values:
-    OUT.write("%s\t" % values)
+    OUT.write("%s\t" % value)
   OUT.write("\n")
 
-
+FH.close()
+CFH.close()
+OUT.close()
   
 
 
