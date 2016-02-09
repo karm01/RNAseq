e6eed6a32f6569774ba414848e7e4e07553327f2
p3p3z <mariom1956@gmail.com>
2016-02-08 22:38:45 -0600

    Modifiqué el script parsing-counts.py. Eliminé algunas de las llamadas al método write de los FILEHANDLER que están dentro de los bucles FOR. Creo que menos llamadas a ese método hace al script más eficiente.

--- scripts/parsing-counts.py
+++ scripts/parsing-counts.py
@@ -13,15 +13,19 @@ for renglon in FH:
     else:
       data[items[0]] = [float(items[3])]
 OUT = open ("conteos-totales.tsv", "w+")
-OUT.write("id\t")
+#OUT.write("id\t")
+first_line = "id\t"
 for cepa in cepas:
-  OUT.write("%s\t" % cepa)
-OUT.write("\n")
+  #OUT.write("%s\t" % cepa)
+  first_line = first_line + "%s\t" % cepa
+OUT.write("%s\n" % first_line)
 for key, values in data.iteritems():
-  OUT.write("%s\t" % key)
+  #OUT.write("%s\t" % key)
+  gene_line = "%s\t" % key
   for value in values:
-    OUT.write("%s\t" % value)
-  OUT.write("\n")
+    #OUT.write("%s\t" % value)
+	gene_line = gene_line + "%s\t" % value
+  OUT.write("%s\n" % gene_line)
 
 FH.close()
 CFH.close()
